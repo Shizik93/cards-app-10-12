e18263f3ec8ea6c9183a09f6a0d2811b149228bb
AleksandrKayuda <akayuda999@gmail.com>
2022-01-03 17:25:10 +0300

    Fix NewPasswordAPI.ts

--- src/API/NewPasswordAPI.ts
+++ src/API/NewPasswordAPI.ts
@@ -7,7 +7,7 @@ export const NewPasswordAPI = {
         return initial.post<AxiosForgotPasswordType>('/auth/forgot', {
             email,
             from: 'AlexKayuda',
-            message: `<div style="background-color: lime; padding: 15px">	error: string;password recovery link: 	}<a href='https://shizik93.github.io/cards-app-10-12/#/new-password/$token$'>link</a></div>`
+            message: `<div style="background-color: lime; padding: 15px"><a href='https://shizik93.github.io/cards-app-10-12/#/new-password/$token$'>Link</a></div>`
         }, {})
     },
     setNewPassword(password: string,resetPasswordToken:string) {
